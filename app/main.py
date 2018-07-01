fb6f35ac11dc2feefe3b17850e1a49cbe2d618e6
Wojtek Stanisławski <stanik.woj@gmail.com>
2018-07-01 21:51:18 +0200

    session id generation

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask import Response
 from flask import render_template
+from flask import request
 import socket
 import json
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
 	hostname = socket.gethostbyname(socket.gethostname())
-	return Response(render_template("main.html", host = hostname), mimetype="text/html")
+	if not "sessionId" in request.cookies:
+		sessId = str(random.randint(100000, 999999))
+	else:
+		sessId = request.cookies["sessionId"]
+	resp = Response(render_template("main.html", host = hostname, sessionId = sessId), mimetype="text/html")
+	if not "sessionId" in request.cookies:
+		resp.set_cookie("sessionId", sessId)
+	return resp
 
 @app.route("/health")
 def health():
