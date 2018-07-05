21adfd59fc19fef0860aef3f64357af49351f30a
Wojtek Stanisławski <stanik.woj@gmail.com>
2018-07-05 23:16:58 +0200

    added redis

--- app/main.py
+++ app/main.py
@@ -5,11 +5,23 @@ from flask import request
 import socket
 import json
 import random
+import redis
+import os
 
 app = Flask(__name__)
+r = redis.StrictRedis(host=os.environ['REDIS_IP'], port=int(os.environ['REDIS_PORT']), db=0)
+
+
+def authenticate():
+	return Response(
+		'Could not verify your access level for that URL.\n'
+		'You have to login with proper credentials', 401,
+		{'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 @app.route("/")
 def main():
+	r.set("foo", "bar")
+	print(r.get("foo"))
 	hostname = socket.gethostbyname(socket.gethostname())
 	if not "sessionId" in request.cookies:
 		sessId = str(random.randint(100000, 999999))
